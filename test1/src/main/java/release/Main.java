c27cf88fa76b670f0c08741409ed7620c4c35a34
Salih Kardan <salih@opsgenie.com>
2017-04-26 09:42:56 +0300

    add modules

---
+++ test1/src/main/java/release/Main.java
@@ -0,0 +1,11 @@
+package release;
+/**
+ * @author Salih Kardan
+ * @version 25/04/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world from test1 module");
+    }
+}
